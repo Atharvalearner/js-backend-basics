7a909bf36d51f82082ccae3d00726b69e0f653d2
Atharvalearner <atharvabanchhode09@gmail.com>
2023-12-15 17:53:58 +0530

    model for todo app is created user,todo,subtodo

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(`'Hello port ${port} is listening.... please give some command'`)
+})
+
+app.get('/instagram',(req,res) => {
+    res.send('instagram is here')
+})
+
+app.get('/whatsapp',(req,res) => {
+    res.send("i am whatsapp")
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
